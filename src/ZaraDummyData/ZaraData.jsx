bc102044bdf2a4bb5d4f13e2ccd0cdb9ad7b189d
attariattari <attariattari549@gmail.com>
2024-02-10 17:31:20 +0500

    muhyo

---
+++ src/ZaraDummyData/ZaraData.jsx
@@ -0,0 +1,6 @@
+export const welcome = {
+  pcimage:
+    "https://static.zara.net/photos///contents/mkt/spots/aw23-studio-collection-maintenance/subhome-xmedia-worldwide//w/1360/IMAGE-landscape-9405796e-d8af-4956-876e-4e6a5a3bd64c-default_0.jpg?ts=1701710022607",
+  mobileimage:
+    "https://static.zara.net/photos///contents/mkt/spots/aw23-studio-collection-maintenance/subhome-xmedia-worldwide//w/628/IMAGE-small-0137d46c-1966-4d5b-9e8f-3d92974f2f44-default_0.jpg?ts=1706544918222",
+};
