f737786d9489752c368a33c9dd878549a3fb34fe
Attariattari <attariattari549@gmail.com>
2024-05-04 23:26:51 +0500

    DONE

--- src/Components/NewAllProducts/ProductsPages/DetailsDisplayProduct.jsx
+++ src/Components/NewAllProducts/ProductsPages/DetailsDisplayProduct.jsx
@@ -41,46 +41,44 @@ function DetailsDisplayProduct() {
     <div className="DetailedProducts">
       <div className="ProductArea cursor-pointer">
         {womenProducts.map((product, index) => (
-          <div
-            className="ProductData"
-            key={index}
-            onClick={() => handleClick(product)}
-          >
-            {renderMedia(product.images)}
-            <div className="AddToCartIcon">
-              <GoPlus />
-            </div>
-            <div className="Detailed">
-              <div className="DetailedTitleandSVG">
-                <h4>
-                  {product.title.length > 25
-                    ? `${product.title.substring(0, 25)}...`
-                    : product.title}
-                </h4>
-                <svg
-                  className="wishlist-icon wishlist-icon--grid"
-                  preserveAspectRatio="xMidYMid slice"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="inherit"
-                  stroke="inherit"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M12 15.238L17 20V4H7v16l5-4.762zm-4 2.429l4-3.81 4 3.81V5H8v12.667z"
-                  ></path>
-                </svg>
+            <div className="ProductGridView" key={index}>
+              <div className="Productproductimage">
+                <img src={product.images} alt="" />
               </div>
-              <div className="PriceSection space-x-3">
-                <span>{product.price.RealPrice}</span>
-                <span>{product.price.discountprice}</span>
+              <div className="AddToCartIcon">
+                <GoPlus />
+              </div>
+              <div className="Detailed">
+                <div className="DetailedTitleandSVG">
+                  <h4>
+                    {product.title.length > 25
+                      ? `${product.title.substring(0, 25)}...`
+                      : product.title}
+                  </h4>
+                  <svg
+                    className="wishlist-icon wishlist-icon--grid"
+                    preserveAspectRatio="xMidYMid slice"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="inherit"
+                    stroke="inherit"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M12 15.238L17 20V4H7v16l5-4.762zm-4 2.429l4-3.81 4 3.81V5H8v12.667z"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="PriceSection space-x-3">
+                  <span>{product.price.RealPrice}</span>
+                  <span>{product.price.discountprice}</span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       <Footer />
     </div>
