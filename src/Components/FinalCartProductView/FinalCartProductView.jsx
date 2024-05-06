852660c927b661cad465168bb66aff7d7d9d2ec7
Attariattari <attariattari549@gmail.com>
2024-05-06 23:43:24 +0500

    Done

--- src/Components/FinalCartProductView/FinalCartProductView.jsx
+++ src/Components/FinalCartProductView/FinalCartProductView.jsx
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
-
+import "./FinalCartProductView.css";
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { ZaraProducts } from "../DummyData/Data";
 const FinalCartProductView = () => {
+  const womenProducts = [
+    ZaraProducts.Women.LINEN_BLEND_ROLL_UP,
+    ZaraProducts.Women.SATINY_BLAZER,
+    ZaraProducts.Women.FITTED_BLAZER,
+    ZaraProducts.Women.ASYMMETRIC_TULLE_DRESS,
+    ZaraProducts.Women.MINIMALIST_FITTED_BLAZER,
+    ZaraProducts.Women.OVERSIZE_CRINKLE,
+    ZaraProducts.Women.OVERSIZE_CRINKLE,
+  ];
+
   const [isScrolled, setIsScrolled] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
@@ -15,12 +32,91 @@ const FinalCartProductView = () => {
   }, []);
   return (
     <div>
-      <div className={`sticky top-0 z-10 ${isScrolled ? "bg-white" : ""}`}>
-        <div className="absolute w-full">
+      <div className="sticky top-0 z-10">
+        <div className={`absolute w-full ${isScrolled ? "bg-white" : ""}`}>
           <Navbar />
         </div>
       </div>
-      FinalCartProductView
+      <div className="FinalCartProductView">
+        <div className="FinalView">
+          <div className="FinalViewTitleFist">
+            WHERE DO YOU WANT TO RECEIVE YOUR ORDER?
+          </div>
+          <div className="FinalViewTitleSecond">DELIVERY ADDRESS</div>
+          <div className="FinalAdrees">
+            <div className="FinalViewTitleForMobile">
+              <p>THEME PARK VIEW LAHORE CHUNG</p>
+              <Link>EDIT</Link>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="inherit"
+                stroke="inherit"
+                class="zds-selection-cell__selection-icon--default cursor-pointer"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M15.336 12 8.624 4.33l.752-.66L16.665 12l-7.289 8.33-.752-.66L15.336 12Z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+          <div className="FinalDataAndDate">
+            <div className="FinalData">
+              <p>ITEMS</p>
+              <div className="Finalslides">
+                <Swiper slidesPerView={3} spaceBetween={0} className="mySwiper">
+                  {womenProducts.map((product, index) => (
+                    <SwiperSlide key={index} className="Swipe">
+                      {" "}
+                      {/* Ensure each slide has a unique key */}
+                      <img
+                        className="FinalSlidesImg"
+                        src={product.images}
+                        alt=""
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+              <div className="FinalDelevryTime">
+                <div className="Detailsfinal">
+                  <input type="checkbox" checked={true} name="" id="" />
+                  <span>THURSDAY 09, MAY - TUESDAY 14, MAY</span>{" "}
+                </div>
+                <span>19.95 EUR</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+      <div className="sticky bottom-0 z-50">
+        <div className="CartpropccessOrder">
+          <div className="CartProccesses">
+            <div>
+              <div>TOTAL</div>
+              <div className="flex-col">
+                <div className="pl-4">899.50 EUR</div>
+                <div
+                  className="text-gray-700"
+                  style={{
+                    fontSize: "9px",
+                  }}
+                >
+                  * BEFORE TAXES
+                </div>
+              </div>
+            </div>
+            <Link className="Cartcontinuebutton" to="/method-selection">
+              <button className="Contiun">CONTINUE</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
